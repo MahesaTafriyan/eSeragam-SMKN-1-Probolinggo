cf983e6660c340dc04e6a37f3b74dbeea2202dc6
MahesaTafriyan <mahesatafrian@gmail.com>
2025-06-04 13:56:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -310,4 +310,4 @@ def add_header(response):
     return response
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT',5000)))
